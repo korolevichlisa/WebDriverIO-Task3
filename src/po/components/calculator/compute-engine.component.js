4579d79e7bea6d3637967ba1c25a7bd7a7ae483f
yelyzaveta.korolevych <korolevich.liza@gmail.com>
2024-05-09 18:03:00 +0300

    added new get

--- src/po/components/calculator/compute-engine.component.js
+++ src/po/components/calculator/compute-engine.component.js
@@ -1,5 +1,11 @@
 export class ComputeEngine{
-    get rootEl() {
-        return $('div.U4lDT')
+   
+    get addGPU() {
+        return $('//div[text()="Add GPUs"]/ancestor::div[contains(@class,"XIfRlb")]/preceding-sibling::div')
+    } 
+
+    get region() {
+        return $('//span[text()="Region"]/ancestor::div[contains(@class, "VfPpkd-O1htCb")]')
     }
+    
 }
